a2acbd9b4e992b45ec16f662030fd8cd25d6ab2e
Ahmet Uysal <auysal16@ku.edu.tr>
2020-05-03 04:26:08 +0300

    Implement Project2 with part A algorithm

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -4,6 +4,73 @@
 #include <stdexcept>
 #include <cstring>
 #include <sys/time.h>
+#include <queue>
+#include <pthread.h>
+
+// type definitions
+class Plane {
+public:
+    unsigned int id;
+    time_t arrival_time;
+
+    Plane(unsigned int _id, time_t _arrival_time) {
+        id = _id;
+        arrival_time = _arrival_time;
+    }
+
+    // lock
+    // condition
+};
+
+class ConcurrentPlaneQueue {
+private:
+    std::queue<Plane> queue;
+    pthread_mutex_t lock{};
+
+public:
+    void push(const Plane &plane) {
+        pthread_mutex_lock(&lock);
+        queue.push(plane);
+        pthread_mutex_unlock(&lock);
+    }
+
+    Plane &front() {
+        pthread_mutex_lock(&lock);
+        Plane &return_val = queue.front();
+        pthread_mutex_unlock(&lock);
+        return return_val;
+    }
+
+    Plane &front_and_pop() {
+        pthread_mutex_lock(&lock);
+        Plane &return_val = queue.front();
+        queue.pop();
+        pthread_mutex_unlock(&lock);
+        return return_val;
+    }
+
+    void pop() {
+        pthread_mutex_lock(&lock);
+        queue.pop();
+        pthread_mutex_unlock(&lock);
+    }
+
+    bool empty() {
+        pthread_mutex_lock(&lock);
+        bool return_val = queue.empty();
+        pthread_mutex_unlock(&lock);
+        return return_val;
+    }
+
+    ConcurrentPlaneQueue() {
+        lock = PTHREAD_MUTEX_INITIALIZER;
+    }
+};
+
+enum PlaneType {
+    LANDING, DEPARTING, EMERGENCY
+};
+
 
 // function declarations
 int pthread_sleep(int seconds);
@@ -11,6 +78,30 @@ int pthread_sleep(int seconds);
 void parse_command_line_arguments(int argc, char *argv[], int *total_simulation_time, double *landing_plane_prob,
                                   unsigned int *random_seed);
 
+void create_plane(PlaneType planeType, time_t currentTime);
+
+void *air_traffic_control_main(void *);
+
+void *landing_plane_main(void *id);
+
+void *departing_plane_main(void *id);
+
+unsigned int generate_unique_plane_id(PlaneType planeType);
+
+double random_double();
+
+// global variables
+ConcurrentPlaneQueue landing_queue;
+ConcurrentPlaneQueue departing_queue;
+ConcurrentPlaneQueue emergency_queue;
+time_t simulation_end_time;
+time_t simulation_start_time;
+pthread_mutex_t first_plane_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+pthread_mutex_t iteration_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t iteration_cond = PTHREAD_COND_INITIALIZER;
+unsigned int current_plane_id = -1;
+FILE *plane_log_file;
 
 /**
  * This program simulates an air traffic controller.
@@ -24,14 +115,44 @@ int main(int argc, char *argv[]) {
     double landing_plane_prob;
     unsigned int random_seed;
     parse_command_line_arguments(argc, argv, &total_simulation_time, &landing_plane_prob, &random_seed);
-    std::cout << total_simulation_time << landing_plane_prob << random_seed;
+    struct timeval tp{};
+    gettimeofday(&tp, nullptr);
+    simulation_start_time = tp.tv_sec;
+    simulation_end_time = tp.tv_sec + total_simulation_time;
+    time_t next_emergency_time = tp.tv_sec + 40;
+    plane_log_file = fopen("./planes.log", "w+");
+    fprintf(plane_log_file, "PlaneID\tStatus\tRequest Time\tRunway Time\tTurnaround Time\n");
+    srand(random_seed);
+
+    pthread_mutex_lock(&first_plane_mutex);
+
+    create_plane(LANDING, tp.tv_sec);
+    create_plane(DEPARTING, tp.tv_sec);
+    pthread_t air_controller_thread;
+    pthread_create(&air_controller_thread, nullptr, air_traffic_control_main, nullptr);
+    pthread_sleep(1);
+    gettimeofday(&tp, nullptr);
+    while (tp.tv_sec < simulation_end_time) {
+        double random_ = random_double();
+        if (random_ <= landing_plane_prob) {
+            create_plane(LANDING, tp.tv_sec);
+        }
+        if (random_ <= 1 - landing_plane_prob) {
+            create_plane(DEPARTING, tp.tv_sec);
+        }
+        if (tp.tv_sec >= next_emergency_time) {
+            create_plane(EMERGENCY, tp.tv_sec);
+            next_emergency_time += 40;
+        }
+        pthread_sleep(1);
+        gettimeofday(&tp, nullptr);
+    }
+
+    fclose(plane_log_file);
     return 0;
 }
 
-
-
 // function implementations
-
 
 /******************************************************************************
  pthread_sleep takes an integer number of seconds to pause the current thread
@@ -62,7 +183,6 @@ int pthread_sleep(int seconds) {
 
     //Upon successful completion, a value of zero shall be returned
     return res;
-
 }
 
 /**
@@ -126,3 +246,112 @@ void parse_command_line_arguments(int argc, char **argv, int *total_simulation_t
     *random_seed = seed;
 }
 
+
+unsigned int generate_unique_plane_id(PlaneType planeType) {
+    static unsigned int nextDepartingId = -1;
+    static unsigned int nextLandingId = -2;
+
+    if (planeType == DEPARTING) {
+        return nextDepartingId += 2;
+    } else {
+        return nextLandingId += 2;
+    }
+}
+
+double random_double() {
+    return (double) rand() / RAND_MAX;
+}
+
+void create_plane(PlaneType planeType, time_t currentTime) {
+
+    unsigned int plane_id = generate_unique_plane_id(planeType);
+    Plane new_plane = Plane(plane_id, currentTime);
+    pthread_t new_plane_thread;
+    unsigned int *plane_id_ptr = (unsigned int *) malloc(sizeof(unsigned int));
+    *plane_id_ptr = plane_id;
+    switch (planeType) {
+        case DEPARTING:
+            departing_queue.push(new_plane);
+            pthread_create(&new_plane_thread, nullptr, departing_plane_main, (void *) plane_id_ptr);
+            break;
+        case LANDING:
+            landing_queue.push(new_plane);
+            pthread_create(&new_plane_thread, nullptr, landing_plane_main, (void *) plane_id_ptr);
+            break;
+        case EMERGENCY:
+            emergency_queue.push(new_plane);
+            pthread_create(&new_plane_thread, nullptr, landing_plane_main, (void *) plane_id_ptr);
+    }
+}
+
+
+void *air_traffic_control_main(void *) {
+    // wait for the first plane
+    pthread_mutex_lock(&first_plane_mutex);
+    struct timeval tp{};
+    gettimeofday(&tp, nullptr);
+    while (tp.tv_sec < simulation_end_time) {
+        if (!emergency_queue.empty()) {
+            printf("Emergency\n");
+            Plane plane = emergency_queue.front_and_pop();
+            current_plane_id = plane.id;
+            pthread_cond_broadcast(&iteration_cond);
+            fprintf(plane_log_file, "%d\t%c\t%ld\t%ld\t%ld\n",
+                    plane.id,
+                    'E',
+                    plane.arrival_time - simulation_start_time,
+                    tp.tv_sec - simulation_start_time,
+                    tp.tv_sec - plane.arrival_time + 2);
+        } else if (!landing_queue.empty()) {
+            printf("Landing\n");
+            Plane plane = landing_queue.front_and_pop();
+            current_plane_id = plane.id;
+            fprintf(plane_log_file, "%d\t%c\t%ld\t%ld\t%ld\n",
+                    plane.id,
+                    'L',
+                    plane.arrival_time - simulation_start_time,
+                    tp.tv_sec - simulation_start_time,
+                    tp.tv_sec - plane.arrival_time + 2);
+        } else {
+            printf("Departing\n");
+            Plane plane = departing_queue.front_and_pop();
+            current_plane_id = plane.id;
+            fprintf(plane_log_file, "%d\t%c\t%ld\t%ld\t%ld\n",
+                    plane.id,
+                    'D',
+                    plane.arrival_time - simulation_start_time,
+                    tp.tv_sec - simulation_start_time,
+                    tp.tv_sec - plane.arrival_time + 2);
+        }
+        pthread_sleep(2);
+        gettimeofday(&tp, nullptr);
+    }
+    pthread_exit(0);
+}
+
+void *landing_plane_main(void *id) {
+    unsigned int my_id = *((unsigned int *) id);
+    if (my_id == 0) {
+        // send first plane signal
+        pthread_mutex_unlock(&first_plane_mutex);
+    }
+
+    // wait for air traffic controller
+    pthread_mutex_lock(&iteration_lock);
+    while (current_plane_id != my_id) {
+        pthread_cond_wait(&iteration_cond, &iteration_lock);
+    }
+    pthread_mutex_unlock(&iteration_lock);
+    pthread_exit(0);
+}
+
+void *departing_plane_main(void *id) {
+    unsigned int my_id = *((unsigned int *) id);
+    // wait for air traffic controller
+    pthread_mutex_lock(&iteration_lock);
+    while (current_plane_id != my_id) {
+        pthread_cond_wait(&iteration_cond, &iteration_lock);
+    }
+    pthread_mutex_unlock(&iteration_lock);
+    pthread_exit(0);
+}
